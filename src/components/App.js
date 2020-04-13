7017c3de5ba20a4e23aa9bfd9ff346aa31c06722
michaldobiezynski <dobiezynski@gmail.com>
2020-04-13 10:47:35 +0100

    styling - themes(SETUP)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+
+import { ThemeProvider } from '@material-ui/styles';
+
 import Header from '../components/ui/Header';
+import theme from './ui/Theme';
 
 function App() {
   return (
-    <div className='App'>
+    <ThemeProvider theme={theme}>
       <Header />
       Hello
-    </div>
+    </ThemeProvider>
   );
 }
 
