c41d45824e53c26947ca4b85e8ce253893060001
michaldobiezynski <dobiezynski@gmail.com>
2020-04-14 11:38:37 +0100

    active tab refresh fix and some header stuff

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -30,7 +30,13 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '3em',
   },
   logo: {
-    height: '7em',
+    height: '8em',
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
   },
   tabContainer: {
     marginLeft: 'auto',
@@ -57,12 +63,35 @@ const Header = () => {
     setValue(value);
   };
 
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === '/estimate' && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
+
   return (
     <Fragment>
       <ElevationScroll>
         <AppBar position='fixed' color='primary'>
           <Toolbar disableGutters>
-            <img alt='company logo' className={classes.logo} src={logo} />
+            <Button
+              disableRipple
+              component={Link}
+              to='/'
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}>
+              <img alt='company logo' className={classes.logo} src={logo} />
+            </Button>
             <Tabs
               value={value}
               onChange={handleChange}
@@ -102,6 +131,8 @@ const Header = () => {
             <Button
               variant='contained'
               color='secondary'
+              component={Link}
+              to='/estimate'
               className={classes.button}>
               Free Estimate
             </Button>
