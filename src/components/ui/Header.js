9eba969972d320c3317014201332fc223cd703d3
michaldobiezynski <dobiezynski@gmail.com>
2020-04-14 11:23:30 +0100

    navigation with react router

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -7,6 +7,7 @@ import Tab from '@material-ui/core/Tab';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo.svg';
 
@@ -67,11 +68,36 @@ const Header = () => {
               onChange={handleChange}
               className={classes.tabContainer}
               indicatorColor='primary'>
-              <Tab className={classes.tab} label='Home' />
-              <Tab className={classes.tab} label='Services' />
-              <Tab className={classes.tab} label='The Revolution' />
-              <Tab className={classes.tab} label='About Us' />
-              <Tab className={classes.tab} label='Contact Us' />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/'
+                label='Home'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/services'
+                label='Services'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/revolution'
+                label='The Revolution'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/about'
+                label='About Us'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/contact'
+                label='Contact Us'
+              />
             </Tabs>
             <Button
               variant='contained'
