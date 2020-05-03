1de4a66fcdf2de9a0b470587037eea8be115bd7d
michaldobiezynski <dobiezynski@gmail.com>
2020-05-03 10:09:29 +0100

    using Menus - navigation

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -159,11 +159,42 @@ const Header = () => {
               open={open}
               onClose={handleClose}
               MenuListProps={{ onMouseLeave: handleClose }}>
-              <MenuItem onClick={handleClose}>
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to='/services'>
+                Services
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to='/customsoftware'>
                 Custom Software Development
               </MenuItem>
-              <MenuItem onClick={handleClose}>Mobile App Development</MenuItem>
-              <MenuItem onClick={handleClose}>Website Development</MenuItem>
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to='/mobileapps'>
+                Mobile App Development
+              </MenuItem>
+              <MenuItem
+                oonClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to='/websites'>
+                Website Development
+              </MenuItem>
             </Menu>
           </Toolbar>
         </AppBar>
